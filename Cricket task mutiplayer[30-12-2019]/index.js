634a30cd457f1644d92165afc462a1c3108c34b0
sushil <39063142+sushilsree98@users.noreply.github.com>
2019-12-31 12:39:55 +0530

    Add files via upload

---
+++ Cricket task mutiplayer[30-12-2019]/index.js
@@ -0,0 +1,150 @@
+const total1= document.querySelector(".team1");
+const total2=document.querySelector(".team2");
+const score1=document.querySelector(".score1");
+const score2=document.querySelector(".score2");
+const team1_button=document.querySelector(".team1_button");
+const team2_button=document.querySelector(".team2_button");
+let counter1=10,counter2=10,playerCount1=1,playerCount2=1,ballCount1=1,ballCount2=1;
+let player1=[];//players in field
+let playerBall1=[];//number of balls per player
+let individualTotal1=0;
+let playerScore1=[];// total score per player
+let player2=[];
+let playerBall2=[];
+let individualTotal2=0;
+let playerScore2=[];
+let firstTeamTotal=[];// runs for each ball
+let secondTeamTotal=[];
+let a,b;//reduce function for each team
+
+
+team1_button.addEventListener('click',getScore1);
+
+function getScore1(){
+  let x=Math.random();
+  // let player1=[];
+  //
+
+  x=(x*10)%7;
+  counter1--;
+  let firstTeamScore = Math.floor(x);
+  score1.innerHTML=firstTeamScore;
+
+    if(counter1>0 && playerCount1<=10){
+      ballCount1++;
+      individualTotal1+=firstTeamScore;
+      if(firstTeamScore===0 || ballCount1===7){
+        playerScore1.push(individualTotal1);
+        individualTotal1=0;
+        player1.push(playerCount1);
+        playerCount1++;
+        playerBall1.push(ballCount1-1);
+        ballCount1=1;
+      }
+       firstTeamTotal.push(firstTeamScore);
+    }else{
+      ballCount1++;
+      individualTotal1+=firstTeamScore;
+      playerScore1.push(individualTotal1);
+      individualTotal1=0;
+      player1.push(playerCount1);
+      playerCount1++;
+      playerBall1.push(ballCount1-1);
+      ballCount1=1;
+      firstTeamTotal.push(firstTeamScore);
+       team1_button.removeEventListener('click',getScore1);
+       team2_button.addEventListener('click',getScore2);
+       document.querySelector(".buttonWinner").removeEventListener('click',winner);
+
+    }
+
+
+
+  console.log(firstTeamTotal);
+  console.log(player1);
+  console.log(playerBall1);
+  console.log(playerScore1);
+   a=firstTeamTotal.reduce(function(total,num){
+    return total+num;
+  });
+  total1.innerHTML="Total Score of Team1 " + a;
+
+
+
+}
+
+
+function getScore2(){
+  let y=Math.random();
+
+  y=(y*10)%7;
+  counter2--;
+  let secondTeamScore = Math.floor(y);
+  score2.innerHTML=secondTeamScore;
+
+    if(counter2>0 && playerCount2<=10){
+      ballCount2++;
+      individualTotal2+=secondTeamScore;
+      if(secondTeamScore===0 || ballCount2===7){
+        playerScore2.push(individualTotal2);
+        individualTotal2=0;
+        player2.push(playerCount2);
+        playerCount2++;
+        playerBall2.push(ballCount2-1);
+        ballCount2=1;
+      }
+       secondTeamTotal.push(secondTeamScore);
+    }else{
+      ballCount2++;
+      individualTotal2+=secondTeamScore;
+      playerScore2.push(individualTotal2);
+      individualTotal2=0;
+      player2.push(playerCount2);
+      playerCount2++;
+      playerBall2.push(ballCount2-1);
+      ballCount2=1;
+       team2_button.removeEventListener('click',getScore2);
+       document.querySelector(".buttonWinner").addEventListener('click',winner);
+      secondTeamTotal.push(secondTeamScore);
+    }
+
+  console.log(secondTeamTotal);
+  console.log(player2);
+  console.log(playerBall2);
+  console.log(playerScore2);
+   b=secondTeamTotal.reduce(function(total,num){
+    return total+num;
+  });
+  total2.innerHTML="Total Score of Team2 " + b;
+
+}
+
+function winner(){
+  if(a>b){
+    document.querySelector(".winner").innerHTML="winner is Team 1";
+  }else if(b==undefined){
+    document.querySelector(".winner").innerHTML=" ";
+  }else if (a===b) {
+    document.querySelector(".winner").innerHTML="Match Draw ";
+  }
+  else{
+    document.querySelector(".winner").innerHTML="Winner is Team 2";
+  }
+
+  localStorage.setItem("team1score",JSON.stringify(firstTeamTotal));
+  localStorage.setItem("team2score",JSON.stringify(secondTeamTotal));
+  localStorage.setItem("total1",JSON.stringify(a));
+  localStorage.setItem("total2",JSON.stringify(b));
+  localStorage.setItem("player1",JSON.stringify(player1));
+  localStorage.setItem("playerBall1",JSON.stringify(playerBall1));
+  localStorage.setItem("playerScore1",JSON.stringify(playerScore1));
+  localStorage.setItem("player2",JSON.stringify(player2));
+  localStorage.setItem("playerBall2",JSON.stringify(playerBall2));
+  localStorage.setItem("playerScore2",JSON.stringify(playerScore2));
+
+
+ setTimeout(function(){
+   location.href="result.html";
+ }, 3000);
+
+}
---
+++ Cricket task mutiplayer[30-12-2019]/index2.js
@@ -0,0 +1,35 @@
+const team_1= document.querySelector(".team_1");
+const team_2= document.querySelector(".team_2");
+let key1=window.localStorage.getItem("team1score");
+let key2=window.localStorage.getItem("team2score");
+let player1=localStorage.getItem("player1");
+let playerBall1=localStorage.getItem("playerBall1");
+let playerScore1=localStorage.getItem("playerScore1");
+let player2= localStorage.getItem("player2");
+let playerBall2=localStorage.getItem("playerBall2");
+let playerScore2=localStorage.getItem("playerScore2");
+player1=JSON.parse(player1);
+playerBall1=JSON.parse(playerBall1);
+playerScore1=JSON.parse(playerScore1);
+player2=JSON.parse(player2);
+playerBall2=JSON.parse(playerBall2);
+playerScore2=JSON.parse(playerScore2);
+
+
+
+team_1.innerHTML="Total score of team1 (" + window.localStorage.getItem("total1")+")  Runs per ball" + key1;
+team_2.innerHTML="Total score of team2 (" + window.localStorage.getItem("total2") +") Runs per ball" + key2;
+
+for(let i=0;i<player1.length;i++){
+  let x=document.createElement("div");
+  x.innerHTML="Player"+"  "+player1[i] + "---"+playerScore1[i] +"("+ playerBall1[i]+")";
+  document.querySelector(".summary1").appendChild(x);
+}
+
+for(let i=0;i<player2.length;i++){
+  let x=document.createElement("div");
+  x.innerHTML="Player"+"  "+player2[i] + "---"+playerScore2[i] +"("+ playerBall2[i]+")";
+  document.querySelector(".summary2").appendChild(x);
+}
+
+window.localStorage.clear();
